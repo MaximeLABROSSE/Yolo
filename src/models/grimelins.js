9bc2fbb5ee7290335919111a9a9b190b6e2648e0
MaximeLABROSSE <45558577+MaximeLABROSSE@users.noreply.github.com>
2018-12-03 12:16:17 +0100

    Delete grimelins.js

--- src/models/grimelins.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    var Grim = sequelize.define('Grim', {
-      username: DataTypes.STRING,
-	  pseudo: DataTypes.STRING,
-	  age: DataTypes.STRING
-    });
-  
-    return Grim;
-  };
